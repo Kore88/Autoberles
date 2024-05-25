417eee14fdcde67d1eee4f478e59b1aafea1702e
Kőrösi Zoltán <prokore88@gmail.com>
2024-05-25 13:31:35 +0200

    Add project files.

---
+++ Model/Car.cs
@@ -0,0 +1,25 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Autoberles.Model {
+    public class Car {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CarID { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public int Price { get; set; }
+
+        public DateTime ResStart { get; set; }
+
+        public DateTime ResEnd { get; set; }
+
+
+    }
+}
